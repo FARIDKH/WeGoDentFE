6ea6169264b2a9623efe445384d7a064d8cd60d1
anarsultani97 <anar.s@code.edu.az>
2023-07-27 21:57:36 +0200

    google tag managers added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,9 @@ const customization = {
     opened: true,
 }
 
+
+const googleTagManagerId = 'GTM-MQ4R7G9V';
+
 export default class MyDocument extends Document {
     render() {
         return (
@@ -26,8 +29,35 @@ export default class MyDocument extends Document {
                     {/* PWA primary color */}
                     <meta name="theme-color" content={theme(customization).palette.primary.main} />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+                    {/* Google Tag Manager Script */}
+                    <script
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','${googleTagManagerId}');
+                        `,
+                        }}
+                    />
+
+                    
+                    {/* End Google Tag Manager (noscript) */}
+
+                    
+
                 </Head>
                 <body>
+                    {/* Google Tag Manager (noscript) */}
+                    <noscript>
+                        <iframe
+                        src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                        />
+                    </noscript>
                     <Main />
                     <NextScript />
                 </body>
